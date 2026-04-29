db240db4cd50780ce1acf2bc85231e0e78550992
Ankit Agrawal <ankitagrawalru@gmail.com>
2026-04-29 18:17:46 +0530

    one more try

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@fusionstrings/swisseph-wasm'],
+  outputFileTracingIncludes: {
+    '/*': [
+      './node_modules/@fusionstrings/swisseph-wasm/script/lib/swisseph_wasm.wasm',
+      './node_modules/@fusionstrings/swisseph-wasm/script/lib/swisseph_wasm.internal.js',
+      './node_modules/@fusionstrings/swisseph-wasm/script/src/wasm_node.js',
+    ],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
